061181dac7ca14ccf177e3804119428bdd074367
fosmjo <imefangjie@gmail.com>
2024-07-11 23:52:05 +0800

    use es module

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const fs = require('node:fs')
-var tus = require('tus-js-client')
+import fs from "fs"
+import * as tus from 'tus-js-client'
+import {FileUrlStorage} from 'tus-js-client/lib.es5/node/index.js'
 
-const path = `${__dirname}/README.md`
+
+// const path = `${__dirname}/README.md`
+const path = "/home/fosmjo/nas/silo/羊毛战记.Silo.S01E01.Freedom.Day.mp4"
 const file = fs.createReadStream(path)
 
 const options = {
   endpoint: 'http://localhost:8080/files/',
+  urlStorage: new FileUrlStorage("url_store.json"),
   metadata: {
-    filename: 'README.md',
-    filetype: 'text/plain',
+    filename: '羊毛战记.Silo.S01E01.Freedom.Day.mp4',
+    filetype: 'viedo/mp4',
   },
   onError(error) {
     console.error('An error occurred:')
@@ -25,4 +29,15 @@ const options = {
 }
 
 const upload = new tus.Upload(file, options)
-upload.start()
+
+
+upload.findPreviousUploads().then(function (previousUploads) {
+    // Found previous uploads so we select the first one.
+    console.log("previousUploads.length:", previousUploads.length)
+    if (previousUploads.length) {
+      upload.resumeFromPreviousUpload(previousUploads[0])
+    }
+
+    // Start the upload
+    upload.start()
+  })
